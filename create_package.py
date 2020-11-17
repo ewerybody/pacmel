4e0fd6030b5471b1dc6650ab4defec4cd3fc8b17
Eric Werner <ewerybody+github@gmail.com>
2020-11-17 16:32:51 +0100

    Update create_package.py

--- create_package.py
+++ create_package.py
@@ -1,7 +1,6 @@
 """
-pacmel package creation script
-
-@author: eric
+pacmel package creation script to prepare deploying itself with itself.
+I love self referencing stuff!
 """
 import os
 import pacmel
@@ -13,19 +12,18 @@ _PACMEL_DIR = os.path.join(_THIS_DIR, 'pacmel')
 
 def pac_yourself():
     """
-    packs the necessary files of itself into a pacmel.mel
+    Pack the necessary files of itself into a `pacmel.mel`.
     """
-    _PACMEL_DIR
-
     # provide a code snippet to execute
     install_code = os.path.join(_THIS_DIR, 'pacmel.install.snippet')
-
     # create a path to target mel file
     mel = os.path.join(_THIS_DIR, 'pacmel.mel')
+
     # and fire it of:
     file_tuple = pacmel.get_dir_file_tuple(_PACMEL_DIR)
     file_tuple.append((os.path.join(_THIS_DIR, 'LICENSE'), os.path.join('pacmel', 'LICENSE')))
     pprint(file_tuple)
+    
     pacmel.pac(file_tuple, mel, install_code)
     print('mel: %s' % mel)
 
